a5bb1edb0c2015c7a6b926985a7d2d73de64072a
Travis-CI <travis@travis-ci.org>
2015-08-02 15:58:15 +0000

    Bump version: 0.0.14 -> 0.0.15
    [ci skip]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import find_packages, setup
 
 setup(name="jobstamps",
-      version="0.0.14",
+      version="0.0.15",
       description="""Cache output of idempotent jobs.""",
       long_description_markdown_filename="README.md",
       author="Sam Spilsbury",
